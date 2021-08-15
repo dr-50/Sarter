7afe03558aa0d926913b6258c8c371a49b29c353
Devon <devonross50@yahoo.com>
2021-08-15 16:45:20 -0500

    updated file structure

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,43 +1,48 @@
-var output = document.getElementById('output');
+var output = document.getElementById("output");
 
-// Admin functions
 // NavBar
 function openNav() {
-    document.getElementById("mySidepanel").style.width = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-  }
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
 // End NavBar
 
 // start of image loader
 
 console.log(output);
-window.addEventListener('load', function() {
-    document.querySelector('input[type="file"]').addEventListener('change', function(){
-        if (this.files && this.files[0]){           
-            var img = document.querySelector('img'); 
-            img.src=URL.createObjectURL(this.files[0]); // set src to file url
-            img.height=200;
-            img.width=200;
-            
-            var importBtn = document.createElement("button");
-            importBtn.id="importBtn";
-            importBtn.textContent="Submit"
-            output.appendChild(importBtn);
-            importBtn.onclick=function(){alert("image would have been loaded if the creater wasn't so slow at building out the functionality")}
-            
-            var importCancelBtn = document.createElement("button");
-            importCancelBtn.id="importCancelBtn";
-            importCancelBtn.textContent="Cancel"
-            output.appendChild(importCancelBtn);
-            importCancelBtn.onclick=function(){
-                location.reload();}
+window.addEventListener("load", function () {
+  document
+    .querySelector('input[type="file"]')
+    .addEventListener("change", function () {
+      if (this.files && this.files[0]) {
+        var img = document.querySelector("img");
+        img.src = URL.createObjectURL(this.files[0]); // set src to file url
+        img.height = 200;
+        img.width = 200;
 
-        }
-    })
-})
+        var importBtn = document.createElement("button");
+        importBtn.id = "importBtn";
+        importBtn.textContent = "Submit";
+        output.appendChild(importBtn);
+        importBtn.onclick = function () {
+          alert(
+            "image would have been loaded if the creater wasn't so slow at building out the functionality"
+          );
+        };
+
+        var importCancelBtn = document.createElement("button");
+        importCancelBtn.id = "importCancelBtn";
+        importCancelBtn.textContent = "Cancel";
+        output.appendChild(importCancelBtn);
+        importCancelBtn.onclick = function () {
+          location.reload();
+        };
+      }
+    });
+});
 // end of image loader
 
 //start of image arranger
@@ -51,22 +56,37 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
-}
+};
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
+
+// Admin functions
 
 
+function login() {
+  var loginName = document.getElementById("username").value;
+  var loginPassword = document.getElementById("password").value;
+
+  console.log(loginName)
+  console.log(loginPassword)
+  if (loginName === "Admin" && loginPassword === "SarterTowing1!") {
+    //console.log("login correct");
+    location.href='./SarterMarineTowing_Admin.html'
+  } else {
+    //console.log("Incorrect login");
+    alert('Incorrect login')
+  }
+};
 // End of Admin functions
-
